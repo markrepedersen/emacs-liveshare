347044dae48648b2c55da86067d5c2317623d5cc
markrepedersen <markrepedersen@gmail.com>
2020-12-25 07:50:01 -0800

    Initial commit

---
+++ liveshare.el
@@ -0,0 +1,34 @@
+;;; liveshare.el --- A frontend for Liveshare, a collaborative, peer-to-peer code editor.
+
+;; Copyright (C) 2020 Mark Pedersen
+
+;; Author: Mark Pedersen <markrepedersen@gmail.com>
+;; Version: 1.0
+;; Package-Requires ()
+;; Keywords: liveshare, editor
+;; URL: https://github.com/markrepedersen/liveshare.el
+
+;;; Commentary:
+
+;; Liveshare is a collaborative code editor that uses a decentralized model so that no centralized server is needed.
+;; Thus, storage of the underlying document is entirely done by each client.
+;; Internally, it uses a Conflict-free Replicated Data Type (CRDT), allowing messages to come in any order.
+
+;; Full documentation is available as an Info manual.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+;;; liveshare.el ends here
